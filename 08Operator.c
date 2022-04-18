c1f3f641e1f7b8f8974a177e4f26cd89ff172d7c
Pallav-007 <103948779+Pallav-007@users.noreply.github.com>
2022-04-18 21:36:51 +0545

    Add files via upload

---
+++ 08Operator.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+/*
+Operator + - * / %
+
+= ->Assignment Operator
+++ ->Increasement Operator
+-- ->Decreasement Operator
+
+&& ->And
+// ->Or
+! ->Not
+
+> ->Is greater than
+< ->Is less than
+<= ->Less Then or equals to
+>= ->Greater than or Equal to
+== ->Is equals to
+
+*/	
+    int a=10;
+	int b=20;
+	int c=30;
+	
+	int  sum= a+b+c;
+	
+	printf("The sum is %d\n",sum);	
+	
+return 0;
+}
